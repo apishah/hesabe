252f78b1cf1e95ce7fa87c512354d7baa66de142
Your Name <you@example.com>
2020-02-29 10:51:45 +0530

    hesabe payment changes

--- models.py
+++ models.py
@@ -7,8 +7,15 @@ from django_extensions.db.models import TimeStampedModel
 # Create your models here.
 
 class Credential(TimeStampedModel):
-    merchant_code = models.IntegerField()
-    base_url = models.CharField(max_length=255)
-    working_key = models.CharField(max_length=455)
-    iv = models.CharField(max_length=455)
+	status_list = ((True,"Enable"),(False,"Disable"))
+	merchant_code = models.IntegerField()
+	working_key = models.CharField(max_length=455)
+	iv = models.CharField(max_length=455)
+	payment_url = models.CharField(max_length=255)
+	success_url = models.CharField(max_length=255)
+	failure_url = models.CharField(max_length=255)
+	knet = models.BooleanField(
+        max_length=50, choices=status_list, verbose_name="Knet Staus", default="Enabaled")
+	mpgs = models.BooleanField(
+        max_length=50, choices=status_list, verbose_name="Mpgs Staus", default="Disabled")
 
--- views.py
+++ views.py
@@ -10,61 +10,83 @@ from .utils import *
 from django.db import transaction as transaction_atomic
 import json
 import requests
+from django.views.decorators.csrf import csrf_exempt
 from .models import Credential
 from django.conf import settings
 
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def hesabe_payment(req,amount,paymentType,args):
+	with transaction_atomic.atomic():
+		print("hisabe")
+		credential_obj = Credential.objects.all()
+		if(len(credential_obj) == 1):
+			args = json.loads(args)
+			merchantCode = credential_obj[0].merchant_code
+			success_url = credential_obj[0].success_url
+			failure_url = credential_obj[0].failure_url
+			working_key = credential_obj[0].working_key
+			iv = credential_obj[0].iv
+			print(args.get("variable1"))
+			print(args)
+			variable1 = args.get("variable1",None)
+			variable2 = args.get("variable2",None)
+			variable3 = args.get("variable3",None)
+			variable4 = args.get("variable4",None)
+			variable5 = args.get("variable5",None)
+			data = {'merchantCode' : merchantCode,"variable1":variable1,"variable2":variable2,"variable3":variable3,
+					"variable4":variable4,"variable5":variable5, "paymentType": paymentType,"version":0,'amount':amount,'responseUrl':success_url,'failureUrl':failure_url }
+			encryptedText = encrypt(str(json.dumps(data)), working_key , iv)
+	        checkoutToken = checkout(encryptedText)
+	        result = decrypt(checkoutToken,working_key , iv)
+	        response = json.loads(result)
+	        decryptToken = response['response']['data']
+	        url = credential_obj[0].payment_url
+	        html = '''\
+	            <html>
+	            <head>
+	                <title>Sub-merchant checkout page</title>
+	                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	            </head>
+	            <body>
+	            <form id="nonseamless" method="get" name="redirect" action='$url'>
+	                    <input type="hidden" id="data" name="data" value='$data'>
+	                    <script language='javascript'>document.redirect.submit();</script>
+	            </form>
+	            </body>
+	            </html>
+	            '''
+	        fin = Template(html).safe_substitute(url=url,data=decryptToken)
+	        return django.http.HttpResponse(fin)
+		
+
+
 @csrf_exempt
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
-def hesabe_payment(req,paymentType=2,amount=0):
-	with transaction_atomic.atomic():
-		credential_obj = Credential.objects.all()
-		if(len(credential_obj)==1):
-
-			amount = amount
-			merchantCode = credential_obj[0].merchant_code
-			baseUrl=credential_obj[0].base_url
-			working_key = credential_obj[0].working_key
-			iv =credential_obj[0].iv
-
-		data = {'merchantCode' : merchantCode, "paymentType": paymentType,"version":0,'amount':amount,'responseUrl':baseUrl+'/response/','failureUrl':baseUrl+'/response/' }
-		encryptedText = encrypt(str(json.dumps(data)), working_key , iv)
-        checkoutToken = checkout(encryptedText)
-        result = decrypt(checkoutToken,working_key , iv)
-        response = json.loads(result)
-        decryptToken = response['response']['data']
-        url = "http://payment-api.eu-central-1.elasticbeanstalk.com/api/payment"
-        html = '''\
-            <html>
-            <head>
-                <title>Sub-merchant checkout page</title>
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-            </head>
-            <body>
-            <form id="nonseamless" method="get" name="redirect" action='$url'>
-                    <input type="hidden" id="data" name="data" value='$data'>
-                    <script language='javascript'>document.redirect.submit();</script>
-            </form>
-            </body>
-            </html>
-            '''
-        fin = Template(html).safe_substitute(url=url,data=decryptToken)
-        return django.http.HttpResponse(fin)
-		# return render(req,'djnago_app/pay.html')
-
-
 def payment(req):
-	return render(req,'djnago_app/pay.html')
+	payment_variables = {
+
+		"variable1" : "abc",
+		"variable2" : "abc",
+		"variable3" : "abc",
+		"variable4" : "abc",
+		"variable5" : "abc",
+		"amount":10
+
+	}
+	return render(req, 'hesabe_app/pay.html',payment_variables)
+
+
+@csrf_exempt
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def knet_payment(req,**kwargs):
+	print(req.POST)
+	return hesabe_payment(req,req.POST.get('amount'), 1 ,req.POST.get('parameter'))
+
+
+def mpgs_payment(req,**kwargs):
+	return hesabe_payment(req,req.POST.get('amount'), 2 ,req.POST.get('parameter'))
 	
-
-
-def knet_payment(req):
-	return hesabe_payment(req,1,amount=10)
-
-
-def mpgs_payment(req):
-	return hesabe_payment(req,2,amount=10)
-
-
 @csrf_exempt
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def response(request):
